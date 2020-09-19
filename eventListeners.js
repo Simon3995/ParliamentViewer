1764ebddf3473bccbf0337260b7c6473808062bd
Irratix <mailvoorfrank.dragt@gmail.com>
2020-09-19 22:27:05 +0200

    refactoring

---
+++ eventListeners.js
@@ -0,0 +1,60 @@
+// make sure highlight it set to null when mouse hovers outside table
+function fixTableEventListeners() {
+	window.addEventListener("mouseover", function(evt) {
+		highlight(null);
+	}, false);
+	document.querySelectorAll('.tablerow').forEach(item => {
+		item.addEventListener("mouseover", function (evt) {
+			evt.stopPropagation();
+		});
+	});
+}
+
+// mousemove event
+window.addEventListener("mousemove", function(evt) {
+	let rect = document.getElementById("myCanvas").getBoundingClientRect();
+	// update mouse coordinates
+	mouseX = evt.clientX - rect.left;
+	mouseY = evt.clientY - rect.top;
+	// sort seats by distance from mouse
+	seats.sort(sortByDistance);
+}, false);
+
+// mousedown event
+window.addEventListener("mousedown", function(evt) {
+	// arrow left
+	if (Math.sqrt((mouseX - 475)**2 + (mouseY - 485)**2) <= 27) {
+		for (let i=0; i<dists.length; i++) {
+			if (dists[i].year < currentYear) {
+				currentYear = dists[i].year;
+				setSeats(dists[i].year);
+				table(dists[i].year);
+				fixTableEventListeners();
+				return;
+			}
+		}
+	}
+	
+	// arrow right
+	if (Math.sqrt((mouseX - 721)**2 + (mouseY - 485)**2) <= 27) {
+		for (let i=dists.length-1; i>=0; i--) {
+			if (dists[i].year > currentYear) {
+				currentYear = dists[i].year;
+				setSeats(dists[i].year);
+				table(dists[i].year);
+				fixTableEventListeners();
+				return;
+			}
+		}
+	}
+}, false);
+
+// fit table in screen height
+window.onload = function() {
+	document.getElementById("table").style.height = window.innerHeight - 560 + "px";
+}
+
+// fit table in screen height
+window.onresize = function() {
+	document.getElementById("table").style.height = window.innerHeight - 560 + "px";
+}
--- partydef.js
+++ partydef.js
@@ -2,224 +2,288 @@ var parties = [];
 
 parties.push({
 	name: "VVD",
-	fullname_nl: "Volkspartij voor Vrijheid en Democratie",
-	fullname_en: "People's Party for Freedom and Democracy",
+	fullname: {
+		nl: "Volkspartij voor Vrijheid en Democratie",
+		en: "People's Party for Freedom and Democracy",
+	},
 	color: "#21276a",
 });
 
 parties.push({
 	name: "CDA",
-	fullname_nl: "Christen-Democratisch App\u00E8l",
-	fullname_en: "Christian Democratic Appeal",
+	fullname: {
+		nl: "Christen-Democratisch App\u00E8l",
+		en: "Christian Democratic Appeal",
+	},
 	color: "#007c5e",
 });
 
 parties.push({
 	name: "D66",
-	fullname_nl: "Democraten 66",
-	fullname_en: "Democrats 66",
+	fullname: {
+		nl: "Democraten 66",
+		en: "Democrats 66",
+	},
 	color: "#01af40",
 });
 
 parties.push({
 	name: "CU",
-	fullname_nl: "ChristenUnie",
-	fullname_en: "Christian Union",
+	fullname: {
+		nl: "ChristenUnie",
+		en: "Christian Union",
+	},
 	color: "#8fd1eb",
 });
 
 parties.push({
 	name: "PVV",
-	fullname_nl: "Partij voor de Vrijheid",
-	fullname_en: "Party for Freedom",
+	fullname: {
+		nl: "Partij voor de Vrijheid",
+		en: "Party for Freedom",
+	},
 	color: "#bfd6ff",
 });
 
 parties.push({
 	name: "GL",
-	fullname_nl: "GroenLinks",
-	fullname_en: "GreenLeft",
+	fullname: {
+		nl: "GroenLinks",
+		en: "GreenLeft",
+	},
 	color: "#50b535",
 });
 
 parties.push({
 	name: "SP",
-	fullname_nl: "Socialistische Partij",
-	fullname_en: "Socialist Party",
+	fullname: {
+		nl: "Socialistische Partij",
+		en: "Socialist Party",
+	},
 	color: "#ed1b24",
 });
 
 parties.push({
 	name: "PvdA",
-	fullname_nl: "Partij van de Arbeid",
-	fullname_en: "Labour Party",
+	fullname: {
+		nl: "Partij van de Arbeid",
+		en: "Labour Party",
+	},
 	color: "#df111b",
 });
 
 parties.push({
 	name: "PvdD",
-	fullname_nl: "Partij voor de Dieren",
-	fullname_en: "Party for the Animals",
+	fullname: {
+		nl: "Partij voor de Dieren",
+		en: "Party for the Animals",
+	},
 	color: "#016b2d",
 });
 
 parties.push({
 	name: "50PLUS",
-	fullname_nl: "50PLUS",
-	fullname_en: "50PLUS",
+	fullname: {
+		nl: "50PLUS",
+		en: "50PLUS",
+	},
 	color: "#92278f",
 });
 
 parties.push({
 	name: "SGP",
-	fullname_nl: "Staatkundig Gereformeerde Partij",
-	fullname_en: "Reformed Political Party",
+	fullname: {
+		nl: "Staatkundig Gereformeerde Partij",
+		en: "Reformed Political Party",
+	},
 	color: "#e95d0e",
 });
 
 parties.push({
 	name: "DENK",
-	fullname_nl: "DENK",
-	fullname_en: "DENK",
+	fullname: {
+		nl: "DENK",
+		en: "DENK",
+	},
 	color: "#38bfc2",
 });
 
 parties.push({
 	name: "FvD",
-	fullname_nl: "Forum voor Democratie",
-	fullname_en: "Forum for Democracy",
+	fullname: {
+		nl: "Forum voor Democratie",
+		en: "Forum for Democracy",
+	},
 	color: "#841818",
 });
 
 parties.push({
 	name: "LPF",
-	fullname_nl: "Lijst Pim Fortuyn",
-	fullname_en: "Pim Fortuyn List",
+	fullname: {
+		nl: "Lijst Pim Fortuyn",
+		en: "Pim Fortuyn List",
+	},
 	color: "#1849db",
 });
 
 parties.push({
 	name: "LN",
-	fullname_nl: "Leefbaar Nederland",
-	fullname_en: "Livable Netherlands",
+	fullname: {
+		nl: "Leefbaar Nederland",
+		en: "Livable Netherlands",
+	},
 	color: "#0a4f8c",
 });
 
 parties.push({
 	name: "RPF",
-	fullname_nl: "Reformatorische Politieke Federatie",
-	fullname_en: "Reformatory Political Federation",
+	fullname: {
+		nl: "Reformatorische Politieke Federatie",
+		en: "Reformatory Political Federation",
+	},
 	color: "#0b2752",
 });
 
 parties.push({
 	name: "GPV",
-	fullname_nl: "Gereformeerd Politiek Verbond",
-	fullname_en: "Reformed Political League",
+	fullname: {
+		nl: "Gereformeerd Politiek Verbond",
+		en: "Reformed Political League",
+	},
 	color: "#fa7c2d",
 });
 
 parties.push({
 	name: "AOV",
-	fullname_nl: "Algemeen Ouderen Verbond",
-	fullname_en: "General Elderly Alliance",
+	fullname: {
+		nl: "Algemeen Ouderen Verbond",
+		en: "General Elderly Alliance",
+	},
 	color: "#287ee0",
 });
 
 parties.push({
 	name: "CD",
-	fullname_nl: "Centrum Democraten",
-	fullname_en: "Centre Democrats",
+	fullname: {
+		nl: "Centrum Democraten",
+		en: "Centre Democrats",
+	},
 	color: "#c9dfff",
 });
 
 parties.push({
 	name: "Unie55+",
-	fullname_nl: "Unie55+",
-	fullname_en: "Union55+",
+	fullname: {
+		nl: "Unie55+",
+		en: "Union55+",
+	},
 	color: "#64cce3",
 });
 
 parties.push({
 	name: "PSP",
-	fullname_nl: "Pacifistisch Socialistische Partij",
-	fullname_en: "Pacifist Socialist Party",
+	fullname: {
+		nl: "Pacifistisch Socialistische Partij",
+		en: "Pacifist Socialist Party",
+	},
 	color: "#f02e2e",
 });
 
 parties.push({
 	name: "PPR",
-	fullname_nl: "Politieke Partij Radikalen",
-	fullname_en: "Political Party of Radicals",
+	fullname: {
+		nl: "Politieke Partij Radikalen",
+		en: "Political Party of Radicals",
+	},
 	color: "#7065ab",
 });
 
 parties.push({
 	name: "CPN",
-	fullname_nl: "Communistische Partij van Nederland",
-	fullname_en: "Communist Party of the Netherlands",
+	fullname: {
+		nl: "Communistische Partij van Nederland",
+		en: "Communist Party of the Netherlands",
+	},
 	color: "#f02e2e",
 });
 
 parties.push({
 	name: "EVP",
-	fullname_nl: "Evangelische Volkspartij",
-	fullname_en: "Evangelical People's Party",
+	fullname: {
+		nl: "Evangelische Volkspartij",
+		en: "Evangelical People's Party",
+	},
 	color: "#d692cd",
 });
 
 parties.push({
 	name: "CP",
-	fullname_nl: "Centrumpartij",
-	fullname_en: "Centre Party",
+	fullname: {
+		nl: "Centrumpartij",
+		en: "Centre Party",
+	},
 	color: "#222222",
 });
 
 parties.push({
 	name: "BP",
-	fullname_nl: "Boerenpartij",
-	fullname_en: "Farmer's Party",
+	fullname: {
+		nl: "Boerenpartij",
+		en: "Farmer's Party",
+	},
 	color: "#333333",
 });
 
 parties.push({
 	name: "DS'70",
-	fullname_nl: "Democratisch Socialisten '70",
-	fullname_en: "Democratic Socialists '70",
+	fullname: {
+		nl: "Democratisch Socialisten '70",
+		en: "Democratic Socialists '70",
+	},
 	color: "#9e008e",
 });
 
 parties.push({
 	name: "KVP",
-	fullname_nl: "Katholieke Volkspartij",
-	fullname_en: "Catholic People's Party",
+	fullname: {
+		nl: "Katholieke Volkspartij",
+		en: "Catholic People's Party",
+	},
 	color: "#3b3736",
 });
 
 parties.push({
 	name: "ARP",
-	fullname_nl: "Anti-Revolutionaire Partij",
-	fullname_en: "Anti-Revolutionary Party",
+	fullname: {
+		nl: "Anti-Revolutionaire Partij",
+		en: "Anti-Revolutionary Party",
+	},
 	color: "#c1d6db",
 });
 
 parties.push({
 	name: "CHU",
-	fullname_nl: "Christelijk-Historische Unie",
-	fullname_en: "Christian Historical Union",
+	fullname: {
+		nl: "Christelijk-Historische Unie",
+		en: "Christian Historical Union",
+	},
 	color: "#1e245c",
 });
 
 parties.push({
 	name: "RKPN",
-	fullname_nl: "Rooms Katholieke Partij Nederland",
-	fullname_en: "Roman Catholic Party of the Netherlands",
+	fullname: {
+		nl: "Rooms Katholieke Partij Nederland",
+		en: "Roman Catholic Party of the Netherlands",
+	},
 	color: "#f0d316",
 });
 
 parties.push({
 	name: "NMP",
-	fullname_nl: "Nieuwe Midden Partij",
-	fullname_en: "New Middle Party",
+	fullname: {
+		nl: "Nieuwe Midden Partij",
+		en: "New Middle Party",
+	},
 	color: "#bddcf0",
 });
