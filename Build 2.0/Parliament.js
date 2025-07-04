6065426dcce433612ceddd73c996a0a310c954b1
shnimon <33228504+Simon3995@users.noreply.github.com>
2025-07-04 18:48:01 +0200

    translated get_seat_centers to javascript

--- Build 2.0/Parliament.js
+++ Build 2.0/Parliament.js
@@ -12,7 +12,26 @@ class Parliament {
         for (let i = 0; i < this.fractions.length; i++) {
             if (this.fractions[i].party.name === name) {
                 this.fractions.splice(i, 1);
+                return;
             }
+        }
+        console.warn(`Removing fraction "${name}" failed!`);
+    }
+
+    seat_amt() {
+        let amt = 0;
+        for (const frac of this.fractions) {
+            amt += frac.seat_amt;
+        }
+        return amt;
+    }
+
+    draw(context) {
+        let seats_centers = get_seats_centers(this.seat_amt());
+
+        let s = 0.01;
+        for (let seat of seats_centers) {
+            ctx._fillRect(seat[0] - s, -seat[1] - s, 2*s, 2*s);
         }
     }
 }
--- Build 2.0/main.js
+++ Build 2.0/main.js
@@ -1,11 +1,13 @@
 let c = document.getElementById("canvas");
 let ctx = TWO.getEnhancedContext(c);
+ctx.setCameraPosition(1, -0.5);
+ctx.setZoom(c.width / 2.1);
 
 let red_party = new Party("RP", "Red Party", "#ff0000");
-let blue_party = new Party("BP", "Blue Party", "#0000ff");
 
 let p = new Parliament();
-p.add_fraction(new Fraction(red_party, 25));
-p.add_fraction(new Fraction(blue_party, 30));
+p.add_fraction(new Fraction(red_party, 400));
+
+p.draw(ctx);
 
 console.log(p);
