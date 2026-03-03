39344227ca49a5e8a8303302c6028ca5c05de983
Simon van Raaij <33228504+Simon3995@users.noreply.github.com>
2026-03-03 19:37:20 +0100

    More US House elections

--- Build 2.0/timelines/us_houseofrepresentatives.js
+++ Build 2.0/timelines/us_houseofrepresentatives.js
@@ -27,4 +27,29 @@ T.add_parliament(new Parliament([
     new Fraction(T.parties.$rep, 199),
 ], "2018 United States House of Representatives Elections", new Date("2018-11-06")));
 
+T.add_parliament(new Parliament([
+    new Fraction(T.parties.$dem, 194),
+    new Fraction(T.parties.$rep, 241),
+], "2016 United States House of Representatives Elections", new Date("2016-11-08")));
+
+T.add_parliament(new Parliament([
+    new Fraction(T.parties.$dem, 188),
+    new Fraction(T.parties.$rep, 247),
+], "2014 United States House of Representatives Elections", new Date("2014-11-04")));
+
+T.add_parliament(new Parliament([
+    new Fraction(T.parties.$dem, 201),
+    new Fraction(T.parties.$rep, 234),
+], "2012 United States House of Representatives Elections", new Date("2012-11-06")));
+
+T.add_parliament(new Parliament([
+    new Fraction(T.parties.$dem, 193),
+    new Fraction(T.parties.$rep, 242),
+], "2010 United States House of Representatives Elections", new Date("2010-11-02")));
+
+T.add_parliament(new Parliament([
+    new Fraction(T.parties.$dem, 257),
+    new Fraction(T.parties.$rep, 178),
+], "2008 United States House of Representatives Elections", new Date("2008-11-04")));
+
 Timelines["us_houseofrepresentatives"] = T;
