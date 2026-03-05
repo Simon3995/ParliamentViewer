d401e93b726114dff91bd225a1bb421208d31f5d
Simon van Raaij <33228504+Simon3995@users.noreply.github.com>
2026-03-05 16:09:49 +0100

    Added US Senate

---
+++ Build 2.0/timelines/us_senate.js
@@ -0,0 +1,18 @@
+T = new Timeline("United States - Senate");
+
+T.parties = {
+    $dem: new Party("Dem", "Democratic Party", "#3360db", new Image()),
+    $rep: new Party("Rep", "Republican Party", "#e02929", new Image()),
+    $ind: new Party("Ind", "Independent", "#808080", new Image()),
+}
+
+T.parties.$dem.image.src = "logos/us/dem.png";
+T.parties.$rep.image.src = "logos/us/rep.png";
+
+T.add_parliament(new Parliament([
+    new Fraction(T.parties.$dem, 45),
+    new Fraction(T.parties.$ind, 2),
+    new Fraction(T.parties.$rep, 53),
+], "2024 United States Senate Elections", new Date("2024-11-05")));
+
+Timelines["us_senate"] = T;
