935248289afc5a017765953c1a40ad1641ee54e7
Simon van Raaij <33228504+Simon3995@users.noreply.github.com>
2026-04-19 20:53:14 +0200

    Added logos for hu_orszaggyules

--- Build 2.0/timelines/hu_orszaggyules.js
+++ Build 2.0/timelines/hu_orszaggyules.js
@@ -1,12 +1,18 @@
 T = new Timeline("hu_orszaggyules");
 
 T.parties = {
-    $tisza: new Party("TISZA", "Tisztelet és Szabadság Párt", "$tisza", "#77cbff", new Image()),
-    $fidesz: new Party("Fidesz", "Fidesz – Magyar Polgári Szövetség", "fidesz", "#ff8000", new Image()),
+    $tisza: new Party("TISZA", "Tisztelet és Szabadság Párt", "$tisza", "#4dbbff", new Image()),
+    $fidesz: new Party("Fidesz", "Fidesz – Magyar Polgári Szövetség", "fidesz", "#FF5800", new Image()),
     $mh: new Party("MH", "Mi Hazánk Mozgalom", "$mh", "#2f9d2f", new Image()),
     $em: new Party("EM", "Egységben Magyarországért", "$em", "#3ae2b2", new Image()),
     $ldu: new Party("LdU", "Landesselbstverwaltung der Ungarndeutschen", "$ldu", "#325699", new Image()),
 }
+
+T.parties.$tisza.image.src = "logos/hu/tisza.png";
+T.parties.$fidesz.image.src = "logos/hu/fidesz.png";
+T.parties.$mh.image.src = "logos/hu/mh.png";
+T.parties.$ldu.image.src = "logos/hu/ldu.png";
+T.parties.$em.image.src = "logos/hu/em.png";
 
 T.add_parliament(new Parliament([
     new Fraction(T.parties.$tisza, 138),
